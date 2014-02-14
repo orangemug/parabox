5f6b691cdee29956f838013302375c87ee7c7a83
orangemug <info@orangemug.co.uk>
2014-02-14 19:11:36 +0000

    Exposed message-transport via public API.

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 module.exports = {
   Client: require("./lib/client"),
   Server: require("./lib/server"),
-  postmessage:  require("./lib/transport/postmessage")
+  postmessage:  require("./lib/transport/postmessage"),
+  messageTransport:  require("./lib/transport/message-transport")
 };
